2c527c193c4c80893867aa506f1c0a1beeafd520
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-04-27 12:30:59 +0530

    Update file1.groovy

--- vars/file1.groovy
+++ vars/file1.groovy
@@ -1,11 +1,25 @@
-def pull(){
-    sh 'sudo docker pull nginx:latest'
+def call(M values) {
+    def globalVars = [:]
+    globalVars.image_name = image-name
+    globalVars.region= region
+    globalVars.ecr_url = ecr_url
+    globalVars.tag = tag
+    globalVars.repo_url= repo_url
+    
+
+    // Now you can use `globalVars.myVar` in other functions
+   pull(globalVars)
+   push(globalvars)
+   
+}
+def pull(values){
+    sh 'sudo docker pull ${globalVars.image_name}:${globalVars.tag}'
     sh 'ls'
 }
 
-def push(){
-    sh 'sudo aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 337764066236.dkr.ecr.ap-south-1.amazonaws.com'
-    sh 'sudo docker tag nginx:latest 337764066236.dkr.ecr.ap-south-1.amazonaws.com/repo1:latest'
-    sh 'sudo docker push 337764066236.dkr.ecr.ap-south-1.amazonaws.com/repo1'
+def push(values){
+    sh 'sudo aws ecr get-login-password --region ${globalVars.region} | sudo docker login --username AWS --password-stdin ${globalVars.ecr_url}'
+    sh 'sudo docker tag ${globalVars.image_name}:${globalVars.tag} ${globalVars.repo_url}:${globalVars.tag}'
+    sh 'sudo docker push ${globalVars.repo_url}'
             
 }
