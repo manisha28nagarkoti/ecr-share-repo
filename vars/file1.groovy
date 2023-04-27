93a0e9eaeaa2715002bb7be46841ce445838ccfd
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-04-27 15:12:06 +0530

    Update file1.groovy

--- vars/file1.groovy
+++ vars/file1.groovy
@@ -1,12 +1,11 @@
-
 def pull(){
-    sh 'sudo docker pull root@${env.image_name}:root@${env.tag}'
+    sh "sudo docker pull ${env.image_name}:${env.tag}"
     sh 'ls'
 }
 
 def push(){
-    sh 'sudo aws ecr get-login-password --region ${env.region} | sudo docker login --username AWS --password-stdin ${env.ecr_url}'
-    sh 'sudo docker tag ${env.image_name}:${env.tag} ${env.repo_url}:${env.tag}'
-    sh 'sudo docker push ${env.repo_url}'
-            
+    sh "sudo aws ecr get-login-password --region ${env.region} | sudo docker login --username AWS --password-stdin ${env.ecr_url}"
+    sh "sudo docker tag ${env.image_name}:${env.tag} ${env.repo_url}:${env.tag}"
+    sh "sudo docker push ${env.repo_url}:${env.tag}"
 }
+
