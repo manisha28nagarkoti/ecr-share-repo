63dadc2fe21129b69a3f4262f9b7ec18fb574966
manisha28nagarkoti <126867552+manisha28nagarkoti@users.noreply.github.com>
2023-04-27 15:02:53 +0530

    Update file1.groovy

--- vars/file1.groovy
+++ vars/file1.groovy
@@ -1,25 +1,12 @@
-def call(M values) {
-    def globalVars = [:]
-    globalVars.image_name = image-name
-    globalVars.region= region
-    globalVars.ecr_url = ecr_url
-    globalVars.tag = tag
-    globalVars.repo_url= repo_url
-    
 
-    // Now you can use `globalVars.myVar` in other functions
-   pull(globalVars)
-   push(globalvars)
-   
-}
 def pull(values){
-    sh 'sudo docker pull ${globalVars.image_name}:${globalVars.tag}'
+    sh 'sudo docker pull ${env.image_name}:${env.tag}'
     sh 'ls'
 }
 
 def push(values){
-    sh 'sudo aws ecr get-login-password --region ${globalVars.region} | sudo docker login --username AWS --password-stdin ${globalVars.ecr_url}'
-    sh 'sudo docker tag ${globalVars.image_name}:${globalVars.tag} ${globalVars.repo_url}:${globalVars.tag}'
-    sh 'sudo docker push ${globalVars.repo_url}'
+    sh 'sudo aws ecr get-login-password --region ${env.region} | sudo docker login --username AWS --password-stdin ${env.ecr_url}'
+    sh 'sudo docker tag ${env.image_name}:${env.tag} ${env.repo_url}:${env.tag}'
+    sh 'sudo docker push ${env.repo_url}'
             
 }
